11569513d5815de5a3b9f7d32600408710578126
Uğur ALTUN <aziz_altun93@hotmail.com>
2025-06-13 01:49:29 +0300

    edits cors

--- config/cors.php
+++ config/cors.php
@@ -2,27 +2,11 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
-    'paths' => ['*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        env('FRONTEND_URL'),
-        'https://crud-front.altuntech.com',
-    ],
+    'allowed_origins' => ['https://crud-front.altuntech.com'],
 
     'allowed_origins_patterns' => [],
 
@@ -32,5 +16,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
+
 ];
